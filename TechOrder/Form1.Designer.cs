0d64585b36c2795b4b68c785b42d07c0e9640d1e
AzureShadow7 <106389001+AzureShadow7@users.noreply.github.com>
2024-10-02 17:05:08 +0100

    prompt made

--- TechOrder/Form1.Designer.cs
+++ TechOrder/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TechOrder
 {
-    partial class Form1
+    partial class TechOrder
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TechOrder));
+            this.WelcomeLabel = new System.Windows.Forms.Label();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.PromptLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // WelcomeLabel
+            // 
+            this.WelcomeLabel.AutoSize = true;
+            this.WelcomeLabel.Font = new System.Drawing.Font("Perpetua Titling MT", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WelcomeLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.WelcomeLabel.Location = new System.Drawing.Point(422, 229);
+            this.WelcomeLabel.Name = "WelcomeLabel";
+            this.WelcomeLabel.Size = new System.Drawing.Size(483, 57);
+            this.WelcomeLabel.TabIndex = 0;
+            this.WelcomeLabel.Text = "Welcome to XXX!";
+            // 
+            // StartButton
+            // 
+            this.StartButton.Font = new System.Drawing.Font("Perpetua Titling MT", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.StartButton.Image = ((System.Drawing.Image)(resources.GetObject("StartButton.Image")));
+            this.StartButton.Location = new System.Drawing.Point(381, 340);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(553, 123);
+            this.StartButton.TabIndex = 1;
+            this.StartButton.Text = "START";
+            this.StartButton.UseVisualStyleBackColor = true;
+            // 
+            // PromptLabel
+            // 
+            this.PromptLabel.AutoSize = true;
+            this.PromptLabel.Font = new System.Drawing.Font("PMingLiU-ExtB", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PromptLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.PromptLabel.Location = new System.Drawing.Point(507, 506);
+            this.PromptLabel.Name = "PromptLabel";
+            this.PromptLabel.Size = new System.Drawing.Size(284, 32);
+            this.PromptLabel.TabIndex = 2;
+            this.PromptLabel.Text = "press START to begin";
+            // 
+            // TechOrder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(1328, 955);
+            this.Controls.Add(this.PromptLabel);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.WelcomeLabel);
+            this.Name = "TechOrder";
+            this.Text = "TechOrder-StartPage";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label WelcomeLabel;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Label PromptLabel;
     }
 }
 
--- TechOrder/Form1.cs
+++ TechOrder/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace TechOrder
 {
-    public partial class Form1 : Form
+    public partial class TechOrder : Form
     {
-        public Form1()
+        public TechOrder()
         {
             InitializeComponent();
         }
